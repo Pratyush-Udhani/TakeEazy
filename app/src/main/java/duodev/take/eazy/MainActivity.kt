becb3ed7366eeb15237e9db548debc2d785d93be
Priyansh-Kedia <kediapriyansh@gmail.com>
2020-08-09 03:22:14 +0530

    added phone auth

--- app/src/main/java/duodev/take/eazy/MainActivity.kt
+++ app/src/main/java/duodev/take/eazy/MainActivity.kt
@@ -1,11 +1,130 @@
 package duodev.take.eazy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    // [START declare_auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
+
+    private var verificationInProgress = false
+    private var storedVerificationId: String? = ""
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            onRestoreInstanceState(savedInstanceState)
+        }
+        init()
+    }
+
+    private fun init() {
+        auth = Firebase.auth
+        setCallbacks()
+    }
+
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+            phoneNumber,
+            60,
+            TimeUnit.SECONDS,
+            this,
+            callbacks)
+    }
+
+    private fun verifyPhoneNumberWithCode(verificationId: String?, code: String) {
+        val credential = PhoneAuthProvider.getCredential(verificationId!!, code)
+        signInWithPhoneAuthCredential(credential)
+    }
+
+    private fun resendVerificationCode(
+        phoneNumber: String,
+        token: PhoneAuthProvider.ForceResendingToken?
+    ) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+            phoneNumber,
+            60,
+            TimeUnit.SECONDS,
+            this,
+            callbacks,
+            token)
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val user = task.result?.user
+                } else {
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                    }
+                }
+            }
+    }
+
+    private fun setCallbacks() {
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                verificationInProgress = false
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                verificationInProgress = false
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                } else if (e is FirebaseTooManyRequestsException) {
+
+                }
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                storedVerificationId = verificationId
+                resendToken = token
+            }
+        }
+        startPhoneNumberVerification("+917891021661")
+    }
+
+    private fun signOut() {
+        auth.signOut()
+    }
+
+    private fun validatePhoneNumber(): Boolean {
+
+        return true
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (verificationInProgress && validatePhoneNumber()) {
+            startPhoneNumberVerification("+917891021661")
+        }
+    }
+
+    override fun onClick(view: View) {
+//                verifyPhoneNumberWithCode(storedVerificationId, code) // To verify phone number
+    }
+
+    companion object {
+
     }
 }
