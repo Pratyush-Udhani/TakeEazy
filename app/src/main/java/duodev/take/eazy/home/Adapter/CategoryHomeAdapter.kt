bb8639e404e27e8858c8734c2db1cac8d10f3a9d
Priyansh-Kedia <kediapriyansh@gmail.com>
2020-08-19 23:05:23 +0530

    added customisation in main frag

--- app/src/main/java/duodev/take/eazy/home/Adapter/CategoryHomeAdapter.kt
+++ app/src/main/java/duodev/take/eazy/home/Adapter/CategoryHomeAdapter.kt
@@ -2,11 +2,13 @@ package duodev.take.eazy.home.Adapter
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import duodev.take.eazy.R
 import duodev.take.eazy.base.BaseRecyclerViewAdapter
+import duodev.take.eazy.utils.*
 
 class CategoryHomeAdapter(
     private val list: MutableList<String>,
@@ -41,6 +43,7 @@ class CategoryHomeAdapter(
 
         private val categoryName: TextView = itemView.findViewById(R.id.category)
         private val categoryCard: CardView = itemView.findViewById(R.id.categoryCard)
+        private val categoryImage: ImageView = itemView.findViewById(R.id.categoryImage)
 
         fun bindItems(category: String) {
             categoryName.text = category
@@ -48,6 +51,20 @@ class CategoryHomeAdapter(
             categoryCard.setOnClickListener {
                 listener.onCategoryClicked(category)
             }
+            val imageDrawable = when(category) {
+                GROCERIES -> R.drawable.ic_groceries
+                MEDICINES -> R.drawable.ic_medicine
+                FRUITS -> R.drawable.ic_fruits_veges
+                BOOKS -> R.drawable.ic_books
+                MEAT -> R.drawable.ic_fish_meat
+                GIFTS -> R.drawable.ic_gifts
+                PET -> R.drawable.ic_pet_supplies
+                HEALTH -> R.drawable.ic_health
+                SWEETS -> R.drawable.ic_sweets
+                ELECTRICAL -> R.drawable.ic_electrical_appliances
+                else -> R.drawable.ic_body_wash
+            }
+            categoryImage.setImageResource(imageDrawable)
         }
     }
 
--- app/src/main/java/duodev/take/eazy/utils/Keys.kt
+++ app/src/main/java/duodev/take/eazy/utils/Keys.kt
@@ -11,3 +11,16 @@ const val CATEGORIES = "categories"
 const val PICK_ITEM_IMAGE = 3
 const val ALLOWED_CHARS = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
 const val ITEM_IMAGES = "itemimages"
+
+
+const val GROCERIES = "Groceries and essentials"
+const val MEDICINES = "Medicines"
+const val FRUITS = "Fruits and vegetables"
+const val BOOKS = "Books and stationary"
+const val GIFTS = "Gifts and lifestyles"
+const val MEAT = "Meat and fish"
+const val PET = "Pet supplies"
+const val HEALTH = "Health and Energy supplements"
+const val SWEETS = "Sweets"
+const val ELECTRICAL = "Electrical appliances"
+const val BODY_WASH = "Body wash and grooming"
