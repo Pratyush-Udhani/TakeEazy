e2640caa6b13a1bba248d50a4f1abde51cab9fe9
Priyansh-Kedia <kediapriyansh@gmail.com>
2020-08-20 00:39:59 +0530

    added store item recycler

--- app/src/main/java/duodev/take/eazy/stores/Adapter/StoreItemSingleAdapter.kt
+++ app/src/main/java/duodev/take/eazy/stores/Adapter/StoreItemSingleAdapter.kt
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.RecyclerView
 import duodev.take.eazy.R
 import duodev.take.eazy.base.BaseRecyclerViewAdapter
 import duodev.take.eazy.pojo.CartItems
-import duodev.take.eazy.pojo.SingleItem
 import duodev.take.eazy.utils.*
 
 class StoreItemSingleAdapter (
@@ -47,9 +46,9 @@ class StoreItemSingleAdapter (
 
         private val itemName: TextView = itemView.findViewById(R.id.itemName)
         private val itemCard: CardView = itemView.findViewById(R.id.itemCard)
-        private val addToCart: CardView = itemView.findViewById(R.id.addToCart)
-        private val addQuantity: ImageView = itemView.findViewById(R.id.addQuantityButton)
-        private val subQuantity: ImageView = itemView.findViewById(R.id.subQuantityButton)
+        private val addToCart: TextView = itemView.findViewById(R.id.addToCart)
+        private val addQuantity: TextView = itemView.findViewById(R.id.addQuantityButton)
+        private val subQuantity: TextView = itemView.findViewById(R.id.subQuantityButton)
         private val editQuantity: LinearLayout = itemView.findViewById(R.id.editQuantityLayout)
         private val itemQuantity: TextView = itemView.findViewById(R.id.quantityText)
 
